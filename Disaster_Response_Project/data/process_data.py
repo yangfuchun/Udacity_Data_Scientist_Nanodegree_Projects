9660e4f3559827ead7b271ca29ef2f8f981d67e7
Fuchun Yang <yangfchn@umich.edu>
2023-06-27 10:35:52 -0700

    Fixed small errors in the *.py documents

--- Disaster_Response_Project/data/process_data.py
+++ Disaster_Response_Project/data/process_data.py
@@ -4,6 +4,27 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def import_data(messages, categories):
+
+    """
+    Imports and preprocesses data from two CSV files, merges them, and performs necessary transformations.
+
+    Args:
+        messages (str): Path to the CSV file containing messages data.
+        categories (str): Path to the CSV file containing categories data.
+
+    Returns:
+        pandas.DataFrame: Merged and preprocessed DataFrame with the following columns:
+            - id: Message ID
+            - message: Message content
+            - original: Original message content (if available)
+            - genre: Genre of the message
+            - category1: First category label (binary, 0 or 1)
+            - category2: Second category label (binary, 0 or 1)
+            ...
+            - categoryN: Nth category label (binary, 0 or 1)
+
+    """
+
     categories = pd.read_csv(categories)
     messages = pd.read_csv(messages)
     df = messages.merge(categories, on='id', how='inner')
@@ -15,19 +36,40 @@ def import_data(messages, categories):
         categories[column] = categories[column].str[-1]
         categories[column] = categories[column].astype(int)
     categories.replace(2, 1, inplace=True)
-    df.drop('categories', axis=1, inplace = True)
+    df.drop('categories', axis=1, inplace=True)
     df = pd.concat([df, categories], axis=1)
     return df
 
 
+
 def clean_data(df):
+    """
+    Removes duplicate rows from the DataFrame.
+
+    Args:
+        df (pandas.DataFrame): Input DataFrame.
+
+    Returns:
+        pandas.DataFrame: DataFrame with duplicate rows removed.
+    """
     df = df.drop_duplicates()
     return df
     
 
 def save_data(df, databaseName):
+    """
+    Saves the DataFrame to an SQLite database.
+
+    Args:
+        df (pandas.DataFrame): DataFrame to be saved.
+        databaseName (str): Name of the SQLite database.
+
+    Returns:
+        None
+    """
     engine = create_engine('sqlite:///'+databaseName)
-    df.to_sql('DisasterResponse', engine,if_exists = 'replace', index=False)  
+    df.to_sql('DisasterResponse', engine, if_exists='replace', index=False)
+ 
 
 def main():
     if len(sys.argv) == 4:
@@ -47,7 +89,12 @@ def main():
         print('Cleaned data saved to database!')
     
     else:
-        print('incorrect! please check again.')
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
 
 
 if __name__ == '__main__':
--- Disaster_Response_Project/models/train_classifier.py
+++ Disaster_Response_Project/models/train_classifier.py
@@ -19,6 +19,17 @@ from sklearn.multioutput import MultiOutputClassifier
 import pickle
 
 def import_data(databaseFile):
+    """
+    Imports data from an SQLite database and splits it into features (X) and labels (y).
+
+    Args:
+        databaseFile (str): Path to the SQLite database file.
+
+    Returns:
+        tuple: A tuple containing the following:
+            - X (pandas.Series): Input messages.
+            - y (pandas.DataFrame): Binary labels for multiple categories.
+    """
     engine = create_engine('sqlite:///'+databaseFile)
     df = pd.read_sql_table('DisasterResponse', engine)
     X = df.message
@@ -26,6 +37,15 @@ def import_data(databaseFile):
     return X, y
 
 def tokenize(text):
+    """
+    Tokenizes and preprocesses the input text.
+
+    Args:
+        text (str): Input text to be tokenized.
+
+    Returns:
+        list: List of clean tokens.
+    """
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
@@ -42,10 +62,16 @@ def tokenize(text):
     return clean_tokens
 
 def build_model():
+    """
+    Builds a machine learning pipeline using CountVectorizer, TfidfTransformer, and MultiOutputClassifier.
+
+    Returns:
+        sklearn.model_selection.GridSearchCV: Grid search model object.
+    """
     pipeline = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('moc', MultiOutputClassifier(RandomForestClassifier())),
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('moc', MultiOutputClassifier(RandomForestClassifier())),
     ])
     parameters = {
         'clf__estimator__n_estimators': [5],
@@ -55,18 +81,40 @@ def build_model():
     return model
 
 def model_eval(model, X_test, y_test):
+    """
+    Evaluates the model's performance by predicting labels for the test set and printing a classification report.
+
+    Args:
+        model: Trained model object.
+        X_test (pandas.Series): Test set features.
+        y_test (pandas.DataFrame): Test set labels.
+
+    Returns:
+        None
+    """
     y_pred = model.predict(X_test)
     print(classification_report(y_test, y_pred))
 
 def save_model(model, modelFile):
+    """
+    Saves the trained model to a file.
+
+    Args:
+        model: Trained model object.
+        modelFile (str): Path to save the model file.
+
+    Returns:
+        None
+    """
     with open(modelFile, 'wb') as file:
         pickle.dump(model, file)
+
 
 
 def main():
     if len(sys.argv) == 3:
         databaseFile, modelFile = sys.argv[1:]
-        print('Importing data...\n    DATABASE: {}'.format(databaseFile))
+        print('Loading data...\n    DATABASE: {}'.format(databaseFile))
         X, y = import_data(databaseFile)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         
@@ -85,8 +133,10 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('incorrect! please check again. ')
-
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 if __name__ == '__main__':
     main()
